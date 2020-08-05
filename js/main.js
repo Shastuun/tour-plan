d16f5bff057da42ed0ba5defa1672a9ae33b89af
Shastuun <shalyov.stepan29@gmail.com>
2020-08-05 22:38:23 +0300

    ВВОД

---
+++ js/main.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    effect: "coverflow",
+  });
+
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
+
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+  var modalButton = $('[data-toggle="modal"]');
+  var closeModalButton = $(".modal__close");
+
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+  //обработка форм
+  $(".modal__form ").validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "Укажите имя",
+        minlength: "Имя должно быть не короче 2 букв",
+      },
+      email: {
+        required: "Укажите адрес электронной почты",
+        email: "Адрес электронной почты должен быть в формате name@domain.com",
+      },
+      phone: "Номер телефона обязателен",
+    },
+  });
+  //обработка форм
+  $(".form ").validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "Укажите имя",
+        minlength: "Имя должно быть не короче 2 букв",
+      },
+      email: {
+        required: "Укажите адрес электронной почты",
+        email: "Адрес электронной почты должен быть в формате name@domain.com",
+      },
+      phone: "Номер телефона обязателен",
+    },
+  });
+});
